c91ec27abdca7c56fb12df65a86542f2c69eb4b5
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2023-07-23 15:33:19 +0900

    実装まで

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,105 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, shallowRef } from 'vue'
+import { useAnimate, type MaybeElement } from '@vueuse/core'
+import GlassShoes from './assets/27161639_m.jpg'
+import GlassShoesFront from './assets/27161639_m_front.png'
+
+const el = shallowRef<MaybeElement>()
+
+const {
+  play,
+  pause,
+  reverse,
+  finish,
+  cancel,
+  startTime,
+  currentTime,
+  playbackRate,
+  playState,
+  replaceState,
+  pending,
+} = useAnimate(
+  el,
+  [
+    { clipPath: 'circle(8% at 0% 30%)' },
+    { clipPath: 'circle(25% at 50% 90%)' },
+    { clipPath: 'circle(3% at 100% 30%)' },
+    { clipPath: 'circle(15% at 0% 70%)' },
+  ],
+  {
+    duration: 6000,
+    iterations: 5,
+    direction: 'alternate',
+    easing: 'cubic-bezier(0.46, 0.03, 0.52, 0.96)',
+  },
+)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="wrapper">
+    <div class="image">
+      <img class="glass-shoes glass-shoes--base" :src="GlassShoes" alt="ガラスのシューズ">
+      <img ref="el" class="glass-shoes glass-shoes--front" :src="GlassShoesFront" alt="ガラスのシューズ">
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="buttons">
+      <button class="buttons__button" @click="play">Play</button>
+      <button class="buttons__button" @click="pause">Pause</button>
+      <button class="buttons__button" @click="reverse">Reverse</button>
+      <button class="buttons__button" @click="cancel">Cancel</button>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+
+<style>
+body {
+  background-color: #bbb;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper {
+  display: grid;
+  place-items: center;
+  height: 100dvh;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.image {
+  width: 700px;
+  max-width: 100%;
+  position: relative;
+  overflow: hidden;
+  border-radius: 20px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.glass-shoes {
+  width: 100%;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.glass-shoes--base {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  filter: brightness(0.2);
+}
+
+.glass-shoes--front {
+  position: relative;
+  z-index: 10;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+
+.buttons__button {
+  background-color: #1a9529;
+  color: white;
+  font-size: 18px;
+  padding: 10px 14px;
+  border-radius: 8px;
+  appearance: none;
+  border: none;
 }
 </style>
